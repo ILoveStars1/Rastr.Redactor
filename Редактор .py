f51f9fbc21a4d844019e7f5f48bfb93c5b33a222
Даниил <sas23092003@gmail.com>
2023-10-10 10:36:59 +0300

    Рисование линий

--- Редактор .py
+++ Редактор .py
@@ -5,16 +5,43 @@ canvas_widht = 1000
 canvas_height = 500
 brush_size = 3
 color = "black"
+mouse_first = True # Если True то первая точка линии
+line_point = None
+lines = []
 
-def paint(event):
-    global brush_size
-    global color
-    x1 = event.x - brush_size
-    x2 = event.x + brush_size
-    y1 = event.y - brush_size
-    y2 = event.y + brush_size
-    w.create_oval (x1, y1, x2, y2,
-                   fill=color, outline=color)
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+
+def add_line(p1, p2):
+    lines.append(Line(p1, p2))
+
+    
+def mouse(event):
+    global mouse_first
+    global line_point
+    x = event.x
+    y = event.y
+    if mouse_first:
+        line_point = (x, y)
+    else:
+        add_line(line_point, (x, y))
+        redraw()
+    mouse_first = not mouse_first
+    #x1 = event.x - brush_size
+    #x2 = event.x + brush_size
+    #y1 = event.y - brush_size
+    #y2 = event.y + brush_size
+
+def redraw():
+    w.delete('all')
+    for line in lines:
+        w.create_line(line.p1[0], line.p1[1], line.p2[0], line.p2[1], fill=color, width=1)
+    #w.create_oval (x1, y1, x2, y2,
+     #              fill=color, outline=color)
         
 def brush_size_change(new_size):
     global brush_size
@@ -26,13 +53,13 @@ def color_change (new_color):
 
 window = Tk()
 window.title("Редактор")
-colorchooser.askcolor()
+#colorchooser.askcolor()
 w = Canvas(window,
             width = canvas_widht,
             height= canvas_height,
             bg = "white")
-w.bind("<B1-Motion>", Line)
-
+w.bind('<Button-1>', mouse)
+#draw()
 w.grid(row=2, column=0,
        columnspan=7, padx=5,
        pady=5, sticky=E+W+S+N)
